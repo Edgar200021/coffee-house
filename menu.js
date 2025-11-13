451fdf96457ad1d39c8953663996ba2232a8accf
Edgar <edgarartur2000@mail.ru>
2025-11-13 15:06:22 +0400

    refactor: change import for json

--- menu.js
+++ menu.js
@@ -1,29 +1,27 @@
-// import products from './products.json' assert { type: 'json' }
-// import * as products from './products.json';
-import("./products.json", { assert: { type: "json" } });
-window.addEventListener('load', () => {
-  const productList = document.querySelector('.menu__list'),
-    tabsList = document.querySelector('.tabs__list'),
-    tabsBtns = Array.from(document.querySelectorAll('.tabs__btn')),
-    container = productList.closest('.container'),
-    refreshBtn = container.querySelector('.menu__refresh-btn'),
-    modal = document.querySelector('.modal')
+import products from "../products.json" with { type: "json" };
 
-  let category = 'coffee',
-    count = window.matchMedia('(max-width: 768px)') ? 4 : 8
+window.addEventListener("load", () => {
+	const productList = document.querySelector(".menu__list"),
+		tabsList = document.querySelector(".tabs__list"),
+		tabsBtns = Array.from(document.querySelectorAll(".tabs__btn")),
+		container = productList.closest(".container"),
+		refreshBtn = container.querySelector(".menu__refresh-btn"),
+		modal = document.querySelector(".modal");
 
-  function renderProducts(category, count = 8) {
-    let productsHtml = ''
+	let category = "coffee",
+		count = window.matchMedia("(max-width: 768px)") ? 4 : 8;
 
-    productList.innerHTML = ''
+	function renderProducts(category, count = 8) {
+		let productsHtml = "";
 
-    const filteredProduct = products.filter(
-      product => product.category === category
-    )
+		productList.innerHTML = "";
 
-    filteredProduct.slice(0, count).forEach(
-      (product, i) =>
-        (productsHtml += `<li class="menu__item card" data-id=${product.id}>
+		const filteredProduct = products.filter(
+			(product) => product.category === category,
+		);
+
+		for (const product of filteredProduct.slice(0, count)) {
+			productsHtml += `<li class="menu__item card" data-id=${product.id}>
 	  <div class="card__img-box">
 		<img
 		  src="./assets/img/drinks/${product.category}-${product.id}.jpg"
@@ -38,89 +36,91 @@ window.addEventListener('load', () => {
 		</p>
 		<span class="card__price">$${product.price}</span>
 	  </div>
-	</li>`)
-    )
+	</li>`;
+		}
 
-    productList.insertAdjacentHTML('beforeend', productsHtml)
+		productList.insertAdjacentHTML("beforeend", productsHtml);
 
-    return filteredProduct.length
-  }
+		return filteredProduct.length;
+	}
 
-  renderProducts(category, count)
+	renderProducts(category, count);
 
-  tabsList.addEventListener('click', async e => {
-    if (!e.target.matches('.tabs__btn')) return
+	tabsList.addEventListener("click", async (e) => {
+		if (!e.target.matches(".tabs__btn")) return;
 
-    const button = e.target
+		const button = e.target;
 
-    tabsBtns.forEach(btn => {
-      btn.classList.remove('btn_tab_inactive')
-      btn.disabled = false
-    })
+		tabsBtns.forEach((btn) => {
+			btn.classList.remove("btn_tab_inactive");
+			btn.disabled = false;
+		});
 
-    button.classList.add('btn_tab_inactive')
-    button.disabled = true
+		button.classList.add("btn_tab_inactive");
+		button.disabled = true;
 
-    category = button.dataset.category
-    count = 4
+		category = button.dataset.category;
+		count = 4;
 
-    const productsListLength = renderProducts(category, count)
+		const productsListLength = renderProducts(category, count);
 
-    refreshBtn.style.display = productsListLength > 4 ? 'flex' : 'none'
-  })
+		refreshBtn.style.display = productsListLength > 4 ? "flex" : "none";
+	});
 
-  const resizeObserver = new ResizeObserver(entries => {
-    const box = entries[0]
+	const resizeObserver = new ResizeObserver((entries) => {
+		const box = entries[0];
 
-    if (box.target.offsetWidth > 768) {
-      renderProducts(category, 8)
-      refreshBtn.style.display = 'none'
-      return
-    }
+		if (box.target.offsetWidth > 768) {
+			renderProducts(category, 8);
+			refreshBtn.style.display = "none";
+			return;
+		}
 
-    const productsListLength = renderProducts(category, count)
-    refreshBtn.style.display =
-      count === 8 || productsListLength <= 4 ? 'none' : 'flex'
-  })
+		const productsListLength = renderProducts(category, count);
+		refreshBtn.style.display =
+			count === 8 || productsListLength <= 4 ? "none" : "flex";
+	});
 
-  resizeObserver.observe(container)
+	resizeObserver.observe(container);
 
-  refreshBtn.addEventListener('click', e => {
-    count += 4
+	refreshBtn.addEventListener("click", (e) => {
+		count += 4;
 
-    renderProducts(category, count)
+		renderProducts(category, count);
 
-    e.target.style.display = 'none'
-  })
+		e.target.style.display = "none";
+	});
 
-  let product
+	let product;
 
-  function createProductModal(e) {
-    const target = e.target.closest('.menu__item')
-    if (!target) return
+	function createProductModal(e) {
+		const target = e.target.closest(".menu__item");
+		if (!target) return;
 
-    const modalInner = modal.querySelector('.modal__inner')
+		const modalInner = modal.querySelector(".modal__inner");
 
-    product = products.find(product => product.id === Number(target.dataset.id))
+		product = products.find(
+			(product) => product.id === Number(target.dataset.id),
+		);
 
-    modal.classList.add('modal_active')
+		modal.classList.add("modal_active");
 
-    const productHtml = `<div class="modal__product single-product" data-id=${
-      product.id
-    }>
+		const productHtml = `<div class="modal__product single-product" data-id=${
+			product.id
+		}>
                 <div class="single-product__img-box">
                   <img
                     src="./assets//img/drinks/${product.category}-${
-      product.id
-    }.jpg"
+											product.id
+										}.jpg"
                     class="single-product__img"
                     alt=${product.name}
                   />
                 </div>
                 <div class="single-product__info">
                   <h2 class="single-product__title third-title">${
-                    product.name
-                  }</h2>
+										product.name
+									}</h2>
                   <p class="single-product__description">
                     ${product.description}
                   </p>
@@ -129,20 +129,20 @@ window.addEventListener('load', () => {
                     <span class="single-product__size-title">Size</span>
                     <div class="single-product__size-container">
 					${Object.entries(product.sizes)
-            .map(([key, value], i) => {
-              return ` <label
-								class="single-product__size-label ${i === 0 && 'active-label'}"
+						.map(([key, value], i) => {
+							return ` <label
+								class="single-product__size-label ${i === 0 && "active-label"}"
 								data-input=${key}
 							>
 								<input
 								class="single-product__size-input"
 								type="radio"
-								value=${value['add-price']}
+								value=${value["add-price"]}
 								/>
 								${value.size}
-							</label>`
-            })
-            .join('')}
+							</label>`;
+						})
+						.join("")}
                     </div>
                   </div>
 
@@ -152,8 +152,8 @@ window.addEventListener('load', () => {
                     >
     				<div class="single-product__ingridients-container">
 					${product.additives
-            .map((value, i) => {
-              return `<label
+						.map((value, i) => {
+							return `<label
 							class="single-product__ingridients-label"
 							data-input=${i + 1}
 						>
@@ -161,21 +161,21 @@ window.addEventListener('load', () => {
 
 							class="single-product__ingridients-input"
 							type="checkbox"
-							value=${value['add-price']}
+							value=${value["add-price"]}
 							/>
 							${value.name}
-						</label>`
-            })
-            .join('')}
-                  
+						</label>`;
+						})
+						.join("")}
+
     				</div>
                   </div>
 
                   <div class="single-product__price">
                     <span class="single-product__price-text">Total:</span>
                     <span class="single-product__price-summary">$${
-                      product.price
-                    }</span>
+											product.price
+										}</span>
                   </div>
 
                   <div class="single-product__download-app">
@@ -188,71 +188,72 @@ window.addEventListener('load', () => {
 
                   <button class="single-product__btn btn">Close</button>
                 </div>
-              </div>`
+              </div>`;
 
-    modalInner.innerHTML = productHtml
-    document.body.style.overflow = 'hidden'
-  }
+		modalInner.innerHTML = productHtml;
+		document.body.style.overflow = "hidden";
+	}
 
-  function closeModal() {
-    modal.classList.remove('modal_active')
-    modal.querySelector('.modal__inner').innerHTML = ''
-    document.body.style.overflow = 'auto'
-  }
+	function closeModal() {
+		modal.classList.remove("modal_active");
+		modal.querySelector(".modal__inner").innerHTML = "";
+		document.body.style.overflow = "auto";
+	}
 
-  productList.addEventListener('click', createProductModal)
+	productList.addEventListener("click", createProductModal);
 
-  let additionalSizePrice = 0,
-    additionalAdditivyPrice = 0
+	let additionalSizePrice = 0,
+		additionalAdditivyPrice = 0;
 
-  modal.addEventListener('click', e => {
-    const target = e.target
-    if (
-      target.matches('.single-product__btn') ||
-      target.matches('.modal__container')
-    ) {
-      closeModal()
-      return
-    }
+	modal.addEventListener("click", (e) => {
+		const target = e.target;
+		if (
+			target.matches(".single-product__btn") ||
+			target.matches(".modal__container")
+		) {
+			closeModal();
+			return;
+		}
 
-    if (target.closest('label')) {
-      const input = target.firstElementChild
-      const priceElement = target
-        .closest('.single-product')
-        .querySelector('.single-product__price-summary')
-      const starterPrice = Number(product.price)
+		if (target.closest("label")) {
+			const input = target.firstElementChild;
+			const priceElement = target
+				.closest(".single-product")
+				.querySelector(".single-product__price-summary");
+			const starterPrice = Number(product.price);
 
-      console.log(input)
-      switch (input.type) {
-        case 'radio':
-          const labels = Array.from(
-            target
-              .closest('.single-product__size-container')
-              .querySelectorAll('label')
-          )
+			console.log(input);
+			switch (input.type) {
+				case "radio": {
+					const labels = Array.from(
+						target
+							.closest(".single-product__size-container")
+							.querySelectorAll("label"),
+					);
 
-          labels.forEach(label => {
-            label.classList.remove('active-label')
-            label.style.pointerEvents = 'auto'
-          })
+					labels.forEach((label) => {
+						label.classList.remove("active-label");
+						label.style.pointerEvents = "auto";
+					});
 
-          target.classList.add('active-label')
-          target.style.pointerEvents = 'none'
+					target.classList.add("active-label");
+					target.style.pointerEvents = "none";
 
-          additionalSizePrice = Number(input.value)
-          break
-        case 'checkbox':
-          target.classList.toggle('active-label')
-          additionalAdditivyPrice = input.checked
-            ? additionalAdditivyPrice - Number(input.value)
-            : additionalAdditivyPrice + Number(input.value)
-          break
-      }
+					additionalSizePrice = Number(input.value);
+					break;
+				}
+				case "checkbox":
+					target.classList.toggle("active-label");
+					additionalAdditivyPrice = input.checked
+						? additionalAdditivyPrice - Number(input.value)
+						: additionalAdditivyPrice + Number(input.value);
+					break;
+			}
 
-      const summaryPrice = additionalAdditivyPrice + additionalSizePrice
+			const summaryPrice = additionalAdditivyPrice + additionalSizePrice;
 
-      priceElement.innerText = '$' + (summaryPrice + starterPrice).toFixed(2)
-      return
-    }
-  })
-})
+			priceElement.innerText = `$${(summaryPrice + starterPrice).toFixed(2)}`;
+			return;
+		}
+	});
+});
--- script.js
+++ script.js
@@ -1,153 +1,153 @@
-window.addEventListener('load', () => {
-  const favorite = document.querySelector('.favorite'),
-    favoriteDrinks = Array.from(
-      favorite.querySelectorAll('.favorite__favorite-drink')
-    ),
-    dots = Array.from(favorite.querySelectorAll('.favorite__slider-dots'))
+window.addEventListener("load", () => {
+	const favorite = document.querySelector(".favorite"),
+		favoriteDrinks = Array.from(
+			favorite.querySelectorAll(".favorite__favorite-drink"),
+		),
+		dots = Array.from(favorite.querySelectorAll(".favorite__slider-dots"));
 
-  dots[0].classList.add('favorite__slider-dots_active')
+	dots[0].classList.add("favorite__slider-dots_active");
 
-  //! Slider //
+	//! Slider //
 
-  let currentSlider = 0,
-    timerMs = 5000,
-    timerMsSecondsPassed = 5
+	let currentSlider = 0,
+		timerMs = 5000,
+		timerMsSecondsPassed = 5;
 
-  function changeSlide(isToLeft) {
-    isToLeft ? currentSlider-- : currentSlider++
+	function changeSlide(isToLeft) {
+		isToLeft ? currentSlider-- : currentSlider++;
 
-    if (currentSlider === -1) {
-      currentSlider = 2
-    }
+		if (currentSlider === -1) {
+			currentSlider = 2;
+		}
 
-    if (currentSlider === 3) {
-      currentSlider = 0
-    }
+		if (currentSlider === 3) {
+			currentSlider = 0;
+		}
 
-    favoriteDrinks.forEach(favoriteDrink => {
-      if (Number(favoriteDrink.dataset['favorite']) === currentSlider + 1) {
-        favoriteDrink.dataset.active = true
-      } else {
-        favoriteDrink.dataset.active = false
-      }
-      favoriteDrink.style.transform = `translateX(-${currentSlider * 100}%)`
-    })
+		favoriteDrinks.forEach((favoriteDrink) => {
+			if (Number(favoriteDrink.dataset["favorite"]) === currentSlider + 1) {
+				favoriteDrink.dataset.active = true;
+			} else {
+				favoriteDrink.dataset.active = false;
+			}
+			favoriteDrink.style.transform = `translateX(-${currentSlider * 100}%)`;
+		});
 
-    dots.forEach(dot => {
-      if (Number(dot.dataset['dots']) === currentSlider + 1) {
-        dot.firstElementChild.style.width = '100%'
-        dot.classList.add('favorite__slider-dots_active')
-      } else {
-        dot.firstElementChild.style.width = 0
-        dot.classList.remove('favorite__slider-dots_active')
-      }
-    })
-  }
+		dots.forEach((dot) => {
+			if (Number(dot.dataset["dots"]) === currentSlider + 1) {
+				dot.firstElementChild.style.width = "100%";
+				dot.classList.add("favorite__slider-dots_active");
+			} else {
+				dot.firstElementChild.style.width = 0;
+				dot.classList.remove("favorite__slider-dots_active");
+			}
+		});
+	}
 
-  function createSliderInterval(ms) {
-    return setInterval(() => {
-      changeSlide()
-    }, ms)
-  }
+	function createSliderInterval(ms) {
+		return setInterval(() => {
+			changeSlide();
+		}, ms);
+	}
 
-  function createSecondsPassedInterval(reset = true) {
-    return setInterval(() => {
-      timerMsSecondsPassed--
+	function createSecondsPassedInterval(reset = true) {
+		return setInterval(() => {
+			timerMsSecondsPassed--;
 
-      if (reset && timerMsSecondsPassed === 0) timerMsSecondsPassed = 5
-    }, 1000)
-  }
+			if (reset && timerMsSecondsPassed === 0) timerMsSecondsPassed = 5;
+		}, 1000);
+	}
 
-  let slideIntervalId = createSliderInterval(timerMs),
-    secondsPasssedIntervalId = createSecondsPassedInterval()
+	let slideIntervalId = createSliderInterval(timerMs),
+		secondsPasssedIntervalId = createSecondsPassedInterval();
 
-  favorite.addEventListener('click', e => {
-    if (!e.target.matches('.favorite__btn')) return
+	favorite.addEventListener("click", (e) => {
+		if (!e.target.matches(".favorite__btn")) return;
 
-    const isToLeft = e.target.matches('.favorite__btn-left')
+		const isToLeft = e.target.matches(".favorite__btn-left");
 
-    clearInterval(slideIntervalId)
-    changeSlide(isToLeft)
+		clearInterval(slideIntervalId);
+		changeSlide(isToLeft);
 
-    slideIntervalId = createSliderInterval(timerMs)
-  })
+		slideIntervalId = createSliderInterval(timerMs);
+	});
 
-  function continueSlide(e) {
-    const activeDot =
-      dots[
-        Number(e.target.closest('[data-active="true"]').dataset['favorite']) - 1
-      ]
+	function continueSlide(e) {
+		const activeDot =
+			dots[
+				Number(e.target.closest('[data-active="true"]').dataset["favorite"]) - 1
+			];
 
-    activeDot.firstElementChild.style.width = '100%'
+		activeDot.firstElementChild.style.width = "100%";
 
-    secondsPasssedIntervalId = createSecondsPassedInterval(false)
+		secondsPasssedIntervalId = createSecondsPassedInterval(false);
 
-    if (timerMsSecondsPassed === 1) {
-      changeSlide()
-      return
-    }
-    slideIntervalId = createSliderInterval(timerMsSecondsPassed * 1000)
-  }
+		if (timerMsSecondsPassed === 1) {
+			changeSlide();
+			return;
+		}
+		slideIntervalId = createSliderInterval(timerMsSecondsPassed * 1000);
+	}
 
-  function stopSlide(e) {
-    const activeDot =
-      dots[
-        Number(e.target.closest('[data-active="true"]').dataset['favorite']) - 1
-      ]
+	function stopSlide(e) {
+		const activeDot =
+			dots[
+				Number(e.target.closest('[data-active="true"]').dataset["favorite"]) - 1
+			];
 
-    activeDot.firstElementChild.style.width =
-      activeDot.firstElementChild.offsetWidth + 'px'
+		activeDot.firstElementChild.style.width =
+			activeDot.firstElementChild.offsetWidth + "px";
 
-    clearInterval(slideIntervalId)
-    clearInterval(secondsPasssedIntervalId)
-  }
+		clearInterval(slideIntervalId);
+		clearInterval(secondsPasssedIntervalId);
+	}
 
-  function changePointerAndTouchEvents(element, value) {
-    element.style.pointerEvents = value
-    element.style['touch-action'] = value
+	function changePointerAndTouchEvents(element, value) {
+		element.style.pointerEvents = value;
+		element.style["touch-action"] = value;
 
-    const parentBlock = element.closest('.favorite')
+		const parentBlock = element.closest(".favorite");
 
-    Array.from(parentBlock.querySelectorAll('.favorite__btn')).forEach(
-      btn => (btn.style.pointerEvents = value)
-    )
-  }
+		Array.from(parentBlock.querySelectorAll(".favorite__btn")).forEach(
+			(btn) => (btn.style.pointerEvents = value),
+		);
+	}
 
-  function startTransition(e) {
-    changePointerAndTouchEvents(e.target, 'none')
+	function startTransition(e) {
+		changePointerAndTouchEvents(e.target, "none");
 
-    clearInterval(slideIntervalId)
-    clearInterval(secondsPasssedIntervalId)
+		clearInterval(slideIntervalId);
+		clearInterval(secondsPasssedIntervalId);
 
-    slideIntervalId = createSliderInterval(timerMs)
+		slideIntervalId = createSliderInterval(timerMs);
 
-    timerMsSecondsPassed = 5
+		timerMsSecondsPassed = 5;
 
-    secondsPasssedIntervalId = createSecondsPassedInterval()
-  }
+		secondsPasssedIntervalId = createSecondsPassedInterval();
+	}
 
-  function endTransition(e) {
-    changePointerAndTouchEvents(e.target, 'auto')
-  }
+	function endTransition(e) {
+		changePointerAndTouchEvents(e.target, "auto");
+	}
 
-  let touchstartX = 0
-  let touchendX = 0
+	let touchstartX = 0;
+	let touchendX = 0;
 
-  favoriteDrinks.forEach(drink => {
-    drink.addEventListener('transitionstart', startTransition)
-    drink.addEventListener('transitionend', endTransition)
-    drink.addEventListener('mouseenter', stopSlide)
-    drink.addEventListener('mouseleave', continueSlide)
-    drink.addEventListener('touchstart', e => {
-      touchstartX = e.changedTouches[0].screenX
-      stopSlide(e)
-    })
-    drink.addEventListener('touchend', continueSlide)
-    drink.addEventListener('touchend', e => {
-      touchendX = e.changedTouches[0].screenX
+	favoriteDrinks.forEach((drink) => {
+		drink.addEventListener("transitionstart", startTransition);
+		drink.addEventListener("transitionend", endTransition);
+		drink.addEventListener("mouseenter", stopSlide);
+		drink.addEventListener("mouseleave", continueSlide);
+		drink.addEventListener("touchstart", (e) => {
+			touchstartX = e.changedTouches[0].screenX;
+			stopSlide(e);
+		});
+		drink.addEventListener("touchend", continueSlide);
+		drink.addEventListener("touchend", (e) => {
+			touchendX = e.changedTouches[0].screenX;
 
-      if (touchendX < touchstartX) changeSlide()
-      if (touchendX > touchstartX) changeSlide(true)
-    })
-  })
-})
+			if (touchendX < touchstartX) changeSlide();
+			if (touchendX > touchstartX) changeSlide(true);
+		});
+	});
+});
